286196e0025cfe172f5b5cadb8ab8d705d7a2a63
Dylan Harness <dharness@uwo.ca>
2015-04-12 07:48:39 -0400

    ..

--- platforms/android/com.phonegap.plugins.facebookconnect/hellocordova-FacebookLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Joe\UltraProjects\pricer\platforms\android\com.phonegap.plugins.facebookconnect\hellocordova-FacebookLib\ant-build\AndroidManifest.xml : \
-C:\Users\Joe\UltraProjects\pricer\platforms\android\com.phonegap.plugins.facebookconnect\hellocordova-FacebookLib\AndroidManifest.xml \
