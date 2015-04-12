7aec0d830dbb5a73b350026c189b0bd9a989bf0f
Casey <caseydoyle64@gmail.com>
2015-04-12 08:36:27 -0400

    sdf

--- platforms/android/com.phonegap.plugins.facebookconnect/hellocordova-FacebookLib/bin/AndroidManifest.xml.d
+++ platforms/android/com.phonegap.plugins.facebookconnect/hellocordova-FacebookLib/bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\Joe\UltraProjects\pricer\platforms\android\com.phonegap.plugins.facebookconnect\hellocordova-FacebookLib\ant-build\AndroidManifest.xml : \
-C:\Users\Joe\UltraProjects\pricer\platforms\android\com.phonegap.plugins.facebookconnect\hellocordova-FacebookLib\AndroidManifest.xml \
+C:\Users\Casey\Documents\University\HackPrinceton\Pricer\pricer\platforms\android\com.phonegap.plugins.facebookconnect\hellocordova-FacebookLib\ant-build\AndroidManifest.xml : \
+C:\Users\Casey\Documents\University\HackPrinceton\Pricer\pricer\platforms\android\com.phonegap.plugins.facebookconnect\hellocordova-FacebookLib\AndroidManifest.xml \
