a2ad4a3197eee79ad8ee9ece30906768f5fa1a15
Dylan Harness <dharness@uwo.ca>
2015-04-13 17:04:56 -0400

    did a restructuring of the code, moved views to views folder, styles to css folder and created transitions.js to handle view changes on a global scale

--- platforms/android/com.phonegap.plugins.facebookconnect/hellocordova-FacebookLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Casey\Documents\University\HackPrinceton\Pricer\pricer\platforms\android\com.phonegap.plugins.facebookconnect\hellocordova-FacebookLib\ant-build\AndroidManifest.xml : \
-C:\Users\Casey\Documents\University\HackPrinceton\Pricer\pricer\platforms\android\com.phonegap.plugins.facebookconnect\hellocordova-FacebookLib\AndroidManifest.xml \
---
+++ www/js/transitions.js
@@ -0,0 +1,66 @@
+/*
+    Transitions is a series of functions for navigating the views of the app.
+    To change views, simply replace the content of #theBody with html from
+    your own template, located in the views folder
+*/
+
+loadView = {
+
+    //-----------------------------------// swiping-view //-----------------------------------//
+
+    'swipe-view': function() {
+        window.username = 'morgan'; //user.value;
+        window.kingCoup.getCoupons();
+
+
+        var strVar = "";
+        strVar += "                    <form>";
+        strVar += "                        <div class=\"ui-field-contain\">";
+        strVar += "                            <center><label for=\"slider\"><img src=\"img\/radius.png\"><\/img><\/label><br>";
+        strVar += "                            <input type=\"range\" name=\"slider\" id=\"slider\" min=\"0\" max=\"100\" value=\"50\" >";
+        strVar += "                            <p>Current Search Radius: <span id = \"slider-value\">50<\/span> mi<\/p><\/center><br><br>";
+        strVar += "                        <\/div>";
+        strVar += "                        <div class=\"ui-field-contain\">";
+        strVar += "                            <center><label for=\"sliderAdventure\"><img src=\"img\/adventurous.png\"><\/img<\/label><br>";
+        strVar += "                            <input type=\"range\" name=\"sliderAdventure\" id=\"sliderAdventure\" min=\"10\" max=\"30\" value=\"20\" step = \"10\">";
+        strVar += "                            <p><span id = \"sliderAdventure-value\">Indiana Jones<\/span><\/p><\/center>";
+        strVar += "                        <\/div>";
+        strVar += "                    <\/form>";
+
+
+        $('#theBody').html("<swipe-view><fizz>" + strVar + "</fizz><image-container id='imageContainer'></image-container></swipe-view>");
+        initRange();
+        initSlider();
+    },
+
+    //-----------------------------------// coupons-view //-----------------------------------//
+
+    'my-coupons-view': function() {
+        var data = ["3", "3e", "3gg", "3csd", "3gw", "399"]
+
+        var strVar = "";
+        strVar += "<coupons-view><list id='couponsList'><\/list><\/coupons-view>";
+        $('#theBody').html(strVar);
+
+        setTimeout(function() {
+            $.post("http://dylandjoegotosanfrancisco.com:3002/getSavedDeals", {
+                username: "morgan"
+            }).done(function(res) {
+                var data = res[0].deals;
+
+                var strVar = "";
+                for (var i = 0; i < data.length; i++) {
+                    // Create the list item:
+                    var imgToShow = data[i].showImageStandardBig;
+                    var dealTitle = data[i].dealTitle;
+                    var dealInfo = data[i].dealInfo.substring(0, 20) + '...';
+
+                    strVar += "<coupon-shell width='400px' height='400px'><couponImage><img = src='" + imgToShow + "'\/><\/couponImage><couponTitle>" + dealTitle + "<\/couponTitle><couponDetails>" + dealInfo + "<\/couponDetails><\/coupon-shell>";
+
+                }
+                $('#couponsList').html(strVar);
+            });
+        }, 1000);
+    }
+
+}
